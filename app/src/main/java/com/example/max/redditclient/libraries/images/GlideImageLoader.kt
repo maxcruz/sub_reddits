a7290bd2b9573d4794dc1ac1a33c163cde318fee
Max Cruz <max.raul@gmail.com>
2017-04-03 15:21:43 -0500

    Update dependecies

--- app/src/main/java/com/example/max/redditclient/libraries/images/GlideImageLoader.kt
+++ app/src/main/java/com/example/max/redditclient/libraries/images/GlideImageLoader.kt
@@ -1,11 +1,9 @@
 package com.example.max.redditclient.libraries.images
 
 import android.content.Context
-import android.widget.FrameLayout
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
-import com.example.max.redditclient.libraries.images.ImageLoader
 
 class GlideImageLoader(val context: Context): ImageLoader {
 
